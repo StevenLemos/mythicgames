bc627f91efb7e8f7ac8b283056cd3038e3cfdad5
StevenLemos <lemos.steven@gmail.com>
2016-11-24 11:22:19 -0800

    added day tabs with times at bottom

--- source/assets/js/all.js
+++ source/assets/js/all.js
@@ -5,4 +5,15 @@
 //= require_tree ./src
 
 // Init
-//= require ./src/init.js
+//= require ./src/init.js 
+
+
+$('.tab-header').on('click', function() {
+  var tabId = $(this).data('tabId');
+  $('.tab-header').removeClass('active');
+  $(this).addClass('active');
+  $('.tab-content').removeClass('active');
+  $('#'+tabId).addClass('active');
+
+});
+
